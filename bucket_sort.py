28e45b640abee7252824caad210bde3f803682db
tangled2096 <birva1@gmail.com>
2016-04-07 00:17:26 +0530

    Added files via upload

---
+++ bucket_sort.py
@@ -0,0 +1,34 @@
+# Bucket Sort
+# Complexity: Best Case O(n); Average Case O(n); Worst Case O(n)
+
+
+def bucket_sort(A):
+
+    """
+    Sorts the list 'a' using Bucket sort algorithm
+    >>> from pydsa import bucket_sort
+    >>> a = [3, 4, 2, 1, 12, 9]
+    >>> bucket_sort(a)
+    [1, 2, 3, 4, 9, 12]
+    """
+    buckets = [[] for x in range(11)]
+    for i, x in enumerate(A):
+        ##buckets[int(x*len(buckets))].append(x)
+	buckets[x//10].append(x)
+    out = []
+    for buck in buckets:
+        out += isort(buck)
+    return out
+    
+def isort(A):
+    if len(A) <= 1: return A
+    i = 1
+    while i < len(A):
+        k = A[i]
+        j = i - 1
+        while j >= 0 and A[j] > k:
+            A[j+1] = A[j]
+            A[j] = k
+            j -= 1      
+        i += 1
+    return A
---
+++ shell_sort.py
@@ -0,0 +1,22 @@
+# Shell sort
+
+def shell_sort(A):
+    
+    """
+        Sorts the list 'A' using Bucket sort algorithm
+        >>> from pydsa import shell_sort
+        >>> A = [3, 4, 2, 1, 12, 9]
+        >>> shell_sort(A)
+        [1, 2, 3, 4, 9, 12]
+    """
+    inc = len(A) / 2
+    while inc:
+        for i in range(len(A)):
+            j = i
+            temp = A[i]
+            while j >= inc and A[j-inc] > temp:
+                A[j] = A[j - inc]
+                j -= inc
+            A[j] = temp
+        inc = inc/2 if inc/2 else (0 if inc==1 else 1)
+    return A
