d431147f519ab97aaa90223b61ef5a41dc85d6b2
tangled2096 <birva1@gmail.com>
2016-04-07 00:37:20 +0530

    Added files via upload

--- pydsa/__init__.py
+++ pydsa/__init__.py
@@ -5,5 +5,7 @@ from .insertion_sort import insertion_sort
 from .merge_sort import merge_sort
 from .quick_sort import quick_sort
 from .sleep_sort import sleep_sort
+from .shell_sort import shell_sort
+from .bucket_sort import bucket_sort
 from .bfs import bfs
 from .binary_search import binary_search
